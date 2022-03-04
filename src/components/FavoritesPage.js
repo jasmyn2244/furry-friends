f598d6e730bd67c4811e8275f204f60007526bda
Jasmyn Hicks <jasmynh44@gmail.com>
2022-03-03 20:25:39 -0700

    Add Container that can render individual favorites

--- src/components/FavoritesPage.js
+++ src/components/FavoritesPage.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import './FavoritesPage.css'
+import FavCard from './FavCard'
 
-function FavoritesPage() {
+function FavoritesPage({ favorites }) {
+
+  const favCards =
+    favorites.map(dog => {
+      return (
+        <FavCard
+          key={dog.id}
+          dog={dog}
+        />
+      )
+    })
+
   return (
-    <h1>Here are all the fovorites</h1>
+    <>
+      <h1>Here are all the fovorites</h1>
+      {favCards}
+    </>
   )
 }
+
 export default FavoritesPage
