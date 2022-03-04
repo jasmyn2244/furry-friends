b5da6b6fdb8015c513a45ae6d4f60e2bc59501e8
Jasmyn Hicks <jasmynh44@gmail.com>
2022-03-03 20:23:32 -0700

    Add api call, pass props

--- src/App.js
+++ src/App.js
@@ -10,20 +10,56 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      allPhotos: [],
-      greyScale: [],
-      sizeFiltered: [],
-      error: null,
+      dog: {},
+      favorites: []
     }
   }
+
+  componentDidMount() {
+    fetch('https://api.thedogapi.com/v1/images/search')
+      .then(response => response.json())
+      .then(data => this.setState({ dog: data[0] }))
+  }
+
+  getNewDog = () => {
+    fetch('https://api.thedogapi.com/v1/images/search')
+      .then(response => response.json())
+      .then(data => this.setState({ dog: data[0] }))
+  }
+
+  addToFavorites = () => {
+    console.log("Does this method work?")
+    //let dog = this.state.dog
+    if (!this.state.favorites.includes(this.state.dog)) {
+      this.setState({ favorites: [...this.state.favorites, this.state.dog] })
+    }
+  }
+
+  // componentDidMount() {
+  //   fetch('https://picsum.photos/v2/list')
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ allPhotos: data }))
+  // }
+
+  // componentDidMount() {
+  //   return fetch('https://fe-cors-proxy.herokuapp.com', {
+  //     headers: {
+  //       "Target-URL": "https://picsum.photos/v2/list",
+  //       "Accept": "application/json"
+  //     }
+  //   })
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ allPhotos: data }))
+  // }
+
 
   render() {
     return (
       <>
-       s */}
+        {console.log("favorites array", this.state.favorites)}
         <Switch>
-          <Route path='/' render={() => <HomePage />} />
-          <Route path='/favorites' render={() => <FavoritesPage />} />
+          <Route exact path='/' render={() => <HomePage dog={this.state.dog} addToFavorites={this.addToFavorites} getNewDog={this.getNewDog} />} />
+          <Route path='/favorites' render={() => <FavoritesPage favorites={this.state.favorites} />} />
           <Route path='*' render={() => <ErrorPage />} />
         </Switch>
       </>
