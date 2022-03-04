93a77ab5a241eb7acb784cd05e1e032ff6b3833f
Jasmyn Hicks <jasmynh44@gmail.com>
2022-03-03 20:32:23 -0700

    Home page has buttons that can generate a new dog or add a dog to favorites

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './HomePage.css';
+import { Link } from 'react-router-dom';
 
-function HomePage() {
+class HomePage extends Component {
+  constructor() {
+    super()
+    this.state = {
+      dog: {}
+    }
+  }
 
-  return (
-    <h1>This are all the great photos</h1>
-  )
+  // componentDidMount() {
+  //   fetch('https://api.thedogapi.com/v1/images/search')
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ dog: data[0] }))
+  // }
+
+  // componentDidMount() {
+  //   return fetch('https://fe-cors-proxy.herokuapp.com', {
+  //     headers: {
+  //       "Target-URL": "https://picsum.photos/v2/list",
+  //       "Accept": "application/json"
+  //     }
+  //   })
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ allPhotos: data }))
+  // }
+
+  // makeCards() {
+  //   return this.state.photos.map(photo => {
+  //     console.log('photo in map', photo)
+  //     return (
+  //       < PhotoCard
+  //         key={photo.id}
+  //         photo={photo}
+  //       // key="photo.id"
+  //       // id="photo.id"
+  //       // url="photo.url"
+  //       // download_url="photo.download_url"
+  //       // height="photo.hight"
+  //       // width="photo.width"
+  //       // author="photo.author"
+  //       />
+  //     )
+  //   })
+  // }
+
+  render() {
+    console.log("props", this.props)
+    return (
+      <>
+        <h1>Furry Friends</h1>
+        <Link to="/favorites">
+          <button>Favorites</button>
+        </Link>
+        <img src={this.props.dog.url} />
+        <button type='submit' onClick={() => this.props.getNewDog()} >Next</button>
+        <button onClick={() => this.props.addToFavorites()}>❤️</button>
+        <button>Doggy Deets</button>
+      </>
+    )
+  }
 }
 
 export default HomePage
