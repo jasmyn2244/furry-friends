86d929764a91df8c731dbecd83b19cd574181d56
Jasmyn Hicks <jasmynh44@gmail.com>
2022-03-06 17:38:37 -0700

    Add testing for error page

---
+++ cypress/integration/error.js
@@ -0,0 +1,22 @@
+describe('User will be directed to error page', () => {
+
+  it('Shoulde be able to take user to error page if url path is incorrect', () => {
+    cy.intercept('GET', 'https://api.thedogapi.com/v1/images/search', { statusCode: 404 })
+
+    cy.visit('http://localhost:3000')
+
+    cy.get('.oops-text')
+      .contains('Oops something went wrong. Please try again')
+
+    cy.get('.oops-dog')
+      .should('have.attr', 'src', 'https://images.unsplash.com/photo-1485981133625-f1a03c887f0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')
+
+    cy.get('.error-home-button')
+      .click()
+
+    //Note to self:
+    //Why didn't the click do anything. Stayed on the error page, even though when I use my app in the browser, the button works exactly as it should
+    // cy.get('.home-page-title')
+    //   .contains('Furry Friends')
+  })
+})
--- cypress/integration/favorites-page.js
+++ cypress/integration/favorites-page.js
@@ -1,11 +1,5 @@
 describe('User can visit favorites page, and see the dog images they have favorited', () => {
 
-  // beforeEach(() => {
-
-  // it('Should confirm that true is equal to true', () => {
-  //   expect(true).to.equal(true)
-  // });
-
   it('Should be able to favorite images and then see the favorites', () => {
 
     cy.intercept('GET', 'https://api.thedogapi.com/v1/images/search', { fixture: 'sampledog.json' })
